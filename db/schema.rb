664f1dd0e6f1663a5a42257beec2961dfc9d58ba
Myo T Kyaw <myotkyaw@gmail.com>
2015-09-14 14:13:59 -0700

    Add email column to todos table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150911163751) do
+ActiveRecord::Schema.define(version: 20150914211218) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
 end
